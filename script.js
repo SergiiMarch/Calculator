153ff50f53811320d31c3ffb8b6e17cdb3145040
Sergii Marchenko <marikmarchelo@gmail.com>
2023-06-23 13:15:54 +0300

    add calc

--- script.js
+++ script.js
@@ -3,13 +3,34 @@ const refs = {
   form: document.getElementById("form"),
   amount: document.getElementById("amount"),
   total: document.getElementById("total"),
+  price: document.getElementById("price"),
+  quantity: document.getElementById("quantity"),
 };
 refs.form.addEventListener("input", handleFormInput);
 
+const data = {
+  price: 0,
+  quantity: 0,
+  calcTotalPrice() {
+    return Number((this.price * this.quantity).toFixed(2));
+  },
+};
+
 function handleFormInput(evt) {
-  console.log(evt.target.value);
   const { target } = evt;
   if (target.name === "quantity") {
     refs.amount.textContent = target.value;
   }
+  target.setAttribute("value", target.value);
+  dataFill();
+  displayData();
+}
+
+function dataFill() {
+  data.price = Number(refs.price.value);
+  data.quantity = Number(refs.quantity.value);
+}
+
+function displayData() {
+  refs.total.textContent = data.calcTotalPrice() + "UAN";
 }
